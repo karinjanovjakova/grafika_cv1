1b33b0c5c76f94b04adc80b0c59013fcaec5a5f5
karinjanovjakova <63422901+karinjanovjakova@users.noreply.github.com>
2021-02-21 20:52:57 +0100

    upload

---
+++ ImageViewer.cpp
@@ -0,0 +1,307 @@
+#include "ImageViewer.h"
+
+ImageViewer::ImageViewer(QWidget* parent)
+	: QMainWindow(parent), ui(new Ui::ImageViewerClass)
+{
+	ui->setupUi(this);
+	ui->groupBox->setVisible(false);
+}
+
+//ViewerWidget functions
+ViewerWidget* ImageViewer::getViewerWidget(int tabId)
+{
+	QScrollArea* s = static_cast<QScrollArea*>(ui->tabWidget->widget(tabId));
+	if (s) {
+		ViewerWidget* vW = static_cast<ViewerWidget*>(s->widget());
+		return vW;
+	}
+	return nullptr;
+}
+ViewerWidget* ImageViewer::getCurrentViewerWidget()
+{
+	return getViewerWidget(ui->tabWidget->currentIndex());
+}
+
+// Event filters
+bool ImageViewer::eventFilter(QObject* obj, QEvent* event)
+{
+	if (obj->objectName() == "ViewerWidget") {
+		return ViewerWidgetEventFilter(obj, event);
+	}
+	return false;
+}
+
+//ViewerWidget Events
+bool ImageViewer::ViewerWidgetEventFilter(QObject* obj, QEvent* event)
+{
+	ViewerWidget* w = static_cast<ViewerWidget*>(obj);
+
+	if (!w) {
+		return false;
+	}
+
+	if (event->type() == QEvent::MouseButtonPress) {
+		ViewerWidgetMouseButtonPress(w, event);
+	}
+	else if (event->type() == QEvent::MouseButtonRelease) {
+		ViewerWidgetMouseButtonRelease(w, event);
+	}
+	else if (event->type() == QEvent::MouseMove) {
+		ViewerWidgetMouseMove(w, event);
+	}
+	else if (event->type() == QEvent::Leave) {
+		ViewerWidgetLeave(w, event);
+	}
+	else if (event->type() == QEvent::Enter) {
+		ViewerWidgetEnter(w, event);
+	}
+	else if (event->type() == QEvent::Wheel) {
+		ViewerWidgetWheel(w, event);
+	}
+
+	return QObject::eventFilter(obj, event);
+}
+void ImageViewer::ViewerWidgetMouseButtonPress(ViewerWidget* w, QEvent* event)
+{
+	QMouseEvent* e = static_cast<QMouseEvent*>(event);
+	if (e->button() == Qt::LeftButton) {
+		w->setFreeDrawBegin(e->pos());
+		w->setFreeDrawActivated(true);
+	}
+}
+void ImageViewer::ViewerWidgetMouseButtonRelease(ViewerWidget* w, QEvent* event)
+{
+	QMouseEvent* e = static_cast<QMouseEvent*>(event);
+	if (e->button() == Qt::LeftButton && w->getFreeDrawActivated()) {
+		w->freeDraw(e->pos(), QPen(Qt::red));
+		w->setFreeDrawActivated(false);
+	}
+}
+void ImageViewer::ViewerWidgetMouseMove(ViewerWidget* w, QEvent* event)
+{
+	QMouseEvent* e = static_cast<QMouseEvent*>(event);
+	if (e->buttons() == Qt::LeftButton && w->getFreeDrawActivated()) {
+		w->freeDraw(e->pos(), QPen(Qt::red));
+		//w->freeDrawDDA(e->pos(), Qt::red);
+		w->setFreeDrawBegin(e->pos());
+	}
+}
+void ImageViewer::ViewerWidgetLeave(ViewerWidget* w, QEvent* event)
+{
+}
+void ImageViewer::ViewerWidgetEnter(ViewerWidget* w, QEvent* event)
+{
+}
+void ImageViewer::ViewerWidgetWheel(ViewerWidget* w, QEvent* event)
+{
+	QWheelEvent* wheelEvent = static_cast<QWheelEvent*>(event);
+}
+
+//ImageViewer Events
+void ImageViewer::closeEvent(QCloseEvent* event)
+{
+	if (QMessageBox::Yes == QMessageBox::question(this, "Close Confirmation", "Are you sure you want to exit?", QMessageBox::Yes | QMessageBox::No))
+	{
+		event->accept();
+	}
+	else {
+		event->ignore();
+	}
+}
+
+//Image functions
+void ImageViewer::openNewTabForImg(ViewerWidget* vW)
+{
+	QScrollArea* scrollArea = new QScrollArea;
+	scrollArea->setWidget(vW);
+
+	scrollArea->setBackgroundRole(QPalette::Dark);
+	scrollArea->setWidgetResizable(true);
+	scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+	scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+	vW->setObjectName("ViewerWidget");
+	vW->installEventFilter(this);
+
+	QString name = vW->getName();
+
+	ui->tabWidget->addTab(scrollArea, name);
+}
+bool ImageViewer::openImage(QString filename)
+{
+	QFileInfo fi(filename);
+
+	QString name = fi.baseName();
+	openNewTabForImg(new ViewerWidget(name, QSize(0, 0)));
+	ui->tabWidget->setCurrentIndex(ui->tabWidget->count() - 1);
+
+	ViewerWidget* w = getCurrentViewerWidget();
+
+	QImage loadedImg(filename);
+	return w->setImage(loadedImg);
+}
+bool ImageViewer::saveImage(QString filename)
+{
+	QFileInfo fi(filename);
+	QString extension = fi.completeSuffix();
+	ViewerWidget* w = getCurrentViewerWidget();
+
+	QImage* img = w->getImage();
+	return img->save(filename, extension.toStdString().c_str());
+}
+void ImageViewer::clearImage()
+{
+	ViewerWidget* w = getCurrentViewerWidget();
+	w->clear();
+}
+
+//Slots
+
+//Tabs slots
+void ImageViewer::on_tabWidget_tabCloseRequested(int tabId)
+{
+	ViewerWidget* vW = getViewerWidget(tabId);
+	delete vW; //vW->~ViewerWidget();
+	ui->tabWidget->removeTab(tabId);
+	if (!isImgOpened()) {
+		ui->groupBox->setVisible(false);
+	}
+}
+void ImageViewer::on_actionRename_triggered()
+{
+	if (!isImgOpened()) {
+		msgBox.setText("No image is opened.");
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.exec();
+		return;
+	}
+	ViewerWidget* w = getCurrentViewerWidget();
+	bool ok;
+	QString text = QInputDialog::getText(this, QString("Rename image"), tr("Image name:"), QLineEdit::Normal, w->getName(), &ok);
+	if (ok && !text.trimmed().isEmpty())
+	{
+		w->setName(text);
+		ui->tabWidget->setTabText(ui->tabWidget->currentIndex(), text);
+	}
+}
+
+//Image slots
+void ImageViewer::on_actionNew_triggered()
+{
+	newImgDialog = new NewImageDialog(this);
+	connect(newImgDialog, SIGNAL(accepted()), this, SLOT(newImageAccepted()));
+	newImgDialog->exec();
+}
+void ImageViewer::newImageAccepted()
+{
+	NewImageDialog* newImgDialog = static_cast<NewImageDialog*>(sender());
+
+	int width = newImgDialog->getWidth();
+	int height = newImgDialog->getHeight();
+	QString name = newImgDialog->getName();
+	openNewTabForImg(new ViewerWidget(name, QSize(width, height)));
+	ui->tabWidget->setCurrentIndex(ui->tabWidget->count() - 1);
+	getCurrentViewerWidget()->nakresliOsi(20);
+	ui->groupBox->setVisible(true);
+
+}
+
+void ImageViewer::on_kresli_clicked() {
+	//qDebug() << "snaha kreslit";
+	int typ, N, dieliky;
+	if (ui->radioButton->isChecked())
+		typ = 0;
+	else if (ui->radioButton_2->isChecked())
+		typ = 1;
+	else if (ui->radioButton_3->isChecked())
+		typ = 2;
+	else{
+		msgBox.setText(u8"Zvo¾te typ krivky.");
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.exec();
+		return;
+	}
+
+	dieliky = ui->spinBox->value();
+
+	if (dieliky%2==1) {
+		dieliky += 1;
+		ui->spinBox->setValue(dieliky);
+		msgBox.setText(u8"Poèet dielikov musí by párny. Zvolený poèet bol automaticky navýšený o 1.");
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.exec();
+		return;
+	}
+	N = ui->spinBox_2->value();
+	if (ui->comboBox->currentIndex() == 0) {			
+		getCurrentViewerWidget()->nakresliSinus(N, dieliky, typ);
+	}
+	else {												
+		getCurrentViewerWidget()->nakresliCosinus(N, dieliky, typ);
+	}
+	
+}
+
+void ImageViewer::on_vymaz_clicked() {
+	
+	getCurrentViewerWidget()->clear();
+	getCurrentViewerWidget()->nakresliOsi(ui->spinBox->value());
+}
+
+void ImageViewer::on_actionOpen_triggered()
+{
+	QString folder = settings.value("folder_img_load_path", "").toString();
+
+	QString fileFilter = "Image data (*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm .*xbm .* xpm);;All files (*)";
+	QString fileName = QFileDialog::getOpenFileName(this, "Load image", folder, fileFilter);
+	if (fileName.isEmpty()) { return; }
+
+	QFileInfo fi(fileName);
+	settings.setValue("folder_img_load_path", fi.absoluteDir().absolutePath());
+
+	if (!openImage(fileName)) {
+		msgBox.setText("Unable to open image.");
+		msgBox.setIcon(QMessageBox::Warning);
+		msgBox.exec();
+	}
+}
+void ImageViewer::on_actionSave_as_triggered()
+{
+	if (!isImgOpened()) {
+		msgBox.setText("No image to save.");
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.exec();
+		return;
+	}
+	QString folder = settings.value("folder_img_save_path", "").toString();
+
+	ViewerWidget* w = getCurrentViewerWidget();
+
+	QString fileFilter = "Image data (*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm .*xbm .* xpm);;All files (*)";
+	QString fileName = QFileDialog::getSaveFileName(this, "Save image", folder + "/" + w->getName(), fileFilter);
+	if (fileName.isEmpty()) { return; }
+
+	QFileInfo fi(fileName);
+	settings.setValue("folder_img_save_path", fi.absoluteDir().absolutePath());
+
+	if (!saveImage(fileName)) {
+		msgBox.setText("Unable to save image.");
+		msgBox.setIcon(QMessageBox::Warning);
+		msgBox.exec();
+	}
+	else {
+		msgBox.setText(QString("File %1 saved.").arg(fileName));
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.exec();
+	}
+}
+void ImageViewer::on_actionClear_triggered()
+{
+	if (!isImgOpened()) {
+		msgBox.setText("No image is opened.");
+		msgBox.setIcon(QMessageBox::Information);
+		msgBox.exec();
+		return;
+	}
+	clearImage();
+}
---
+++ ViewerWidget.cpp
@@ -0,0 +1,227 @@
+#include   "ViewerWidget.h"
+#include <QtMath>
+
+ViewerWidget::ViewerWidget(QString viewerName, QSize imgSize, QWidget* parent)
+	: QWidget(parent)
+{
+	setAttribute(Qt::WA_StaticContents);
+	setMouseTracking(true);
+	name = viewerName;
+	if (imgSize != QSize(0, 0)) {
+		img = new QImage(imgSize, QImage::Format_ARGB32);
+		img->fill(Qt::white);
+		resizeWidget(img->size());
+		setPainter();
+	}
+}
+ViewerWidget::~ViewerWidget()
+{
+	delete painter;
+	delete img;
+}
+void ViewerWidget::resizeWidget(QSize size)
+{
+	this->resize(size);
+	this->setMinimumSize(size);
+	this->setMaximumSize(size);
+}
+
+//Image functions
+bool ViewerWidget::setImage(const QImage& inputImg)
+{
+	if (img != nullptr) {
+		delete img;
+	}
+	img = new QImage(inputImg);
+	if (!img) {
+		return false;
+	}
+	resizeWidget(img->size());
+	setPainter();
+	update();
+
+	return true;
+}
+bool ViewerWidget::isEmpty()
+{
+	if (img->size() == QSize(0, 0)) {
+		return true;
+	}
+	return false;
+}
+
+//Draw functions
+void ViewerWidget::freeDraw(QPoint end, QPen pen)
+{
+	painter->setPen(pen);
+	painter->drawLine(freeDrawBegin, end);
+
+	update();
+}
+
+void ViewerWidget::clear()
+{
+	img->fill(Qt::white);
+	update();
+}
+
+//Slots
+void ViewerWidget::paintEvent(QPaintEvent* event)
+{
+	QPainter painter(this);
+	QRect area = event->rect();
+	painter.drawImage(area, *img, area);
+}
+
+//dorobene
+
+void ViewerWidget::nakresliOsi(int a) {
+	QPen pen;
+	painter->setPen(pen);
+	int w = img->width();
+	int h = img->height();
+	QPoint xs(5,h/2), xk(w-5,h/2), ys(w/2,5), yk(w/2, h - 5);
+	painter->drawLine(xs, xk);
+	painter->drawLine(ys, yk);
+	
+	int dielik = (w - 10) / a;
+	int dieliky = (h - 10) / a;
+	int i, x1 = w / 2, x2 = w / 2, y1 = h / 2, y2 = h / 2;
+
+	for (i = 0; i < a/2; i++) {
+		x1 += dielik;
+		x2 += -dielik;
+		y1 += dieliky;
+		y2 += -dieliky;
+		painter->drawLine(x1, h / 2 + 5, x1, h / 2 - 5);
+		painter->drawLine(x2, h / 2 + 5, x2, h / 2 - 5);
+		painter->drawLine(w / 2 - 5, y1, w / 2 + 5, y1);
+		painter->drawLine(w / 2 - 5, y2, w / 2 + 5, y2);
+	};
+}
+
+void ViewerWidget::nakresliCosinus(int N, int dieliky, int typ) {
+	clear();
+	nakresliOsi(dieliky);
+
+	QPen pen;
+	pen.setWidth(2);
+	pen.setColor("red");
+	painter->setPen(pen);
+
+	double a = (N* M_PI)/(dieliky/2);
+	int i, x1 = img->width() / 2, x2 = img->width() / 2;
+	int dielik = (img->width() - 10) / dieliky;
+
+	if (typ == 0) {
+		pen.setWidth(5);
+		painter->setPen(pen);
+		for (i = 0; i < dieliky / 2 + 1; i++) {
+			painter->drawPoint(x1, -(img->height() - 5) / 2 * qCos(i * a) + img->height() / 2);
+			painter->drawPoint(x2, -(img->height() - 5) / 2 * qCos(-i * a) + img->height() / 2);
+			x1 += dielik;
+			x2 += -dielik;
+		}
+	}
+	if (typ == 1) {
+		QPoint A, B, C, D;
+		for (i = 0; i < dieliky / 2 + 1; i++) {
+			if (i == 0) {
+				A.setX(x1);
+				A.setY(-(img->height() - 5) / 2 * qCos(i * a) + img->height() / 2);
+				B.setX(x2);
+				B.setY(-(img->height() - 5) / 2 * qCos(-i * a) + img->height() / 2);
+				x1 += dielik;
+				x2 += -dielik;
+			}
+			else {
+				C.setX(A.x());
+				C.setY(A.y());
+				D.setX(B.x());
+				D.setY(B.y());
+				A.setX(x1);
+				A.setY(-(img->height()-5) / 2 * qCos(i * a) + img->height() / 2);
+				B.setX(x2);
+				B.setY(-(img->height() - 5) / 2 * qCos(-i * a) + img->height() / 2);
+				painter->drawLine(A, C);
+				painter->drawLine(B, D);
+				x1 += dielik;
+				x2 += -dielik;
+			}
+		}
+	}
+	if( typ==2 ) {
+		x1 = img->width() / 2 - dielik / 2;
+		x2 = img->width() / 2 - dielik / 2;
+		for (i = 0; i < dieliky / 2 + 1; i++) {
+			painter->fillRect(x1, img->height() / 2, dielik, -(img->height() - 5) / 2 * qCos(i * a), Qt::red);
+			painter->fillRect(x2, img->height() / 2, dielik, -(img->height() - 5) / 2 * qCos(-i * a), Qt::red);
+			x1 += dielik;
+			x2 += -dielik;
+		}
+	}
+	update();
+}
+
+void ViewerWidget::nakresliSinus(int N, int dieliky, int typ) {
+	clear();
+	nakresliOsi(dieliky);
+
+	QPen pen;
+	pen.setWidth(2);
+	pen.setColor("red");
+	painter->setPen(pen);
+
+	double a = (N * M_PI) / (dieliky / 2);
+	int i, x1 = img->width() / 2, x2 = img->width() / 2;
+	int dielik = (img->width() - 10) / dieliky;
+
+	if (typ == 0) {
+		pen.setWidth(5);
+		painter->setPen(pen);
+		for (i = 0; i < dieliky / 2 + 1; i++) {
+			painter->drawPoint(x1, -(img->height() - 5) / 2 * qSin(i * a) + img->height() / 2);
+			painter->drawPoint(x2, -(img->height() - 5) / 2 * qSin(-i * a) + img->height() / 2);
+			x1 += dielik;
+			x2 += -dielik;
+		}
+	}
+	if (typ == 1) {
+		QPoint A, B, C, D;
+		for (i = 0; i < dieliky / 2 + 1; i++) {
+			if (i == 0) {
+				A.setX(x1);
+				A.setY(-(img->height() - 5) / 2 * qSin(i * a) + img->height() / 2);
+				B.setX(x2);
+				B.setY(-(img->height() - 5) / 2 * qSin(-i * a) + img->height() / 2);
+				x1 += dielik;
+				x2 += -dielik;
+			}
+			else {
+				C.setX(A.x());
+				C.setY(A.y());
+				D.setX(B.x());
+				D.setY(B.y());
+				A.setX(x1);
+				A.setY(-(img->height() - 5) / 2 * qSin(i * a) + img->height() / 2);
+				B.setX(x2);
+				B.setY(-(img->height() - 5) / 2 * qSin(-i * a) + img->height() / 2);
+				painter->drawLine(A, C);
+				painter->drawLine(B, D);
+				x1 += dielik;
+				x2 += -dielik;
+			}
+		}
+	}
+	if( typ==2 ) {
+		x1 = img->width() / 2 - dielik / 2;
+		x2 = img->width() / 2 - dielik / 2;
+		for (i = 0; i < dieliky / 2 + 1; i++) {
+			painter->fillRect(x1, img->height() / 2, dielik, -(img->height() - 5) / 2 * qSin(i * a), Qt:: red);
+			painter->fillRect(x2, img->height() / 2, dielik, -(img->height() - 5) / 2 * qSin(-i * a), Qt:: red);
+			x1 += dielik;
+			x2 += -dielik;
+		}
+	}
+	update();
+}
